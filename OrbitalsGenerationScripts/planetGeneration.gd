1d71c84ce3425795d0ad7cf6a647455cdb1fb524
Bryan Moore <moorebryan21@gmail.com>
2023-06-18 23:58:24 -0500

    fixing things

--- OrbitalsGenerationScripts/planetGeneration.gd
+++ OrbitalsGenerationScripts/planetGeneration.gd
@@ -8,88 +8,65 @@ var posYHalfSize = posXHalfSize
 @onready var timeKeeper = get_node("/root/Main/Time Keeper")
 
 var Planet = preload("res://Classes/Planet.gd")
-#var PlanetSprite = preload("res://")
 
-var propertyMap = {
-	"mass_kg": "Mass (kg)",
-	"diameter_km": "Diameter (km)",
-	"density_kg_m3": "Density (kg/m3)",
-	"gravity_m_s2": "Gravity (m/s2)",
-	"escape_velocity_km_s": "Escape Velocity (km/s)",
-	"rotation_period_hours": "Rotation Period (hours)",
-	"length_of_day_hours": "Length of Day (hours)",
-	"distance_from_sun_km": "Distance from Sun (km)",
-	"perihelion_km": "Perihelion (km)",
-	"aphelion_km": "Aphelion (km)",
-	"orbital_period_days": "Orbital Period (days)",
-	"orbital_velocity_km_s": "Orbital Velocity (km/s)",
-	"orbital_inclination_degrees": "Orbital Inclination (degrees)",
-	"orbital_eccentricity": "Orbital Eccentricity",
-	"obliquity_to_orbit_degrees": "Obliquity to Orbit (degrees)",
-	"mean_temperature_celsius": "Mean Temperature (C)",
-	"surface_pressure_bars": "Surface Pressure (bars)",
-	"number_of_moons": "Number of Moons",
-	"ring_system": "Ring System",
-	"global_magnetic_field": "Global Magnetic Field"
-}
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var planet = Planet.new()
-	if debug:
-		if timeKeeper:
-			print("Time Keeper found")
-			print(timeKeeper.time)
 	var planetInput = FileReader.read_file("res://sol_planets.json")
+
 	for planetName in planetInput:
-		var planetData = planetInput[planetName]
-		
-		#object creation
+		var planet = Planet.new()  # Create a new instance of the Planet class
+		var data = planetInput[planetName]  # Get the data for this planet from the JSON file
+
+		# Set the properties of the planet instance
 		planet.name = planetName
-		#object location
-		var planetXPosition = planetData["Distance from Sun (km)"]
-		#BUG: orbit_radius not defined
+		planet.mass_kg = data["Mass (kg)"]
+		planet.diameter_km = data["Diameter (km)"]
+		planet.density_kg_m3 = data["Density (kg/m3)"]
+		planet.gravity_m_s2 = data["Gravity (m/s2)"]
+		planet.escape_velocity_km_s = data["Escape Velocity (km/s)"]
+		planet.rotation_period_hours = data["Rotation Period (hours)"]
+		planet.length_of_day_hours = data["Length of Day (hours)"]
+		planet.distance_from_sun_km = data["Distance from Sun (km)"]
+		planet.perihelion_km = data["Perihelion (km)"]
+		planet.aphelion_km = data["Aphelion (km)"]
+		planet.orbital_period_days = data["Orbital Period (days)"]
+		planet.orbital_velocity_km_s = data["Orbital Velocity (km/s)"]
+		planet.orbital_inclination_degrees = data["Orbital Inclination (degrees)"]
+		planet.orbital_eccentricity = data["Orbital Eccentricity"]
+		planet.obliquity_to_orbit_degrees = data["Obliquity to Orbit (degrees)"]
+		planet.mean_temperature_celsius = data["Mean Temperature (C)"]
+		planet.surface_pressure_bars = data["Surface Pressure (bars)"]
+		planet.number_of_moons = data["Number of Moons"]
+		planet.ring_system = data["Ring System"] == "Yes"
+		planet.global_magnetic_field = data["Global Magnetic Field"] == "Yes"
+
+		# object location
+		var planetXPosition = data["Distance from Sun (km)"]
 		planet.orbit_radius = planetXPosition / posXHalfSize
-		#TODO: position.y implementation need to implement time function first
-		#var timeKeeper = get_node("/root/Main/Time Keeper")
-		planet.position.y = 0
-		
-		#sprite creation
+
+		# Calculate the position of the planet
+		var angle = randf_range(0, 2 * PI)  # Random angle in radians
+		planet.position.x = planet.orbit_radius * cos(angle)
+		planet.position.y = planet.orbit_radius * sin(angle)
+
+		# sprite creation
 		var sprite = createSprite("res://Graphics/—Pngtree—blue solid color round_5487770.png", planetName)
 		planet.add_child(sprite)
-		#sprite scaling
-		var radius = planetData["Diameter (km)"]
+
+		# sprite scaling
+		var radius = data["Diameter (km)"]
 		var scale_factor = Vector2(radius, radius) / radiusHalfSize
-		#TODO: provide better scaling mechanics for the image scaling 
-		#this is seperate to the whole solar system object scaling
 		sprite.scale *= scale_factor
-		
-		#tree manipulation
+		# tree manipulation
 		self.add_child(planet)
 		Logger.log_node(get_tree().root)
-		
-		#rest
+
+		# rest
 		if debug: 
 			print("Planet name: " + str(planet.name))
 			print("Planet position: " + str(planet.position))
 			print("Planet sprite scale: " + str(sprite.scale))
 			print("=====================")
-			
-	#log exports node tree
-	#Logger.log_node(get_tree().root)
-
-func createObject(ObjectClass, objectData):
-	print(ObjectClass)
-	var newObject = ObjectClass.new()
-	for property in propertyMap.keys():
-		var key = propertyMap[property]
-		var value = objectData[key]
-		# Special handling for boolean values
-		if property == "ring_system" or property == "global_magnetic_field":
-			value = value == "Yes"
-		newObject.set(property, value)
-		#BUG: orbit_angle not defined
-	newObject.orbit_angle = 0.0
-	return newObject
 
 func createSprite(imagePath, name):
 	var sprite = Sprite2D.new()
--- OrbitalsGenerationScripts/timeKeeper.gd
+++ OrbitalsGenerationScripts/timeKeeper.gd
@@ -11,12 +11,15 @@ func advanceTime(amount):
 	movePlanets(amount)
 
 func movePlanets(amount):
+	print(planetContainer.get_children())
 	for planet in planetContainer.get_children():
 		var newPosition = Vector2()
+		print(planet.orbit_radius, planet.orbit_angle)
 		planet.orbit_angle += amount
 		newPosition.x = planet.orbit_radius * cos(planet.orbit_angle)
 		newPosition.y = planet.orbit_radius * sin(planet.orbit_angle)
 		planet.position = newPosition
+		
 		if debug:
 			print(planet.position)
 
